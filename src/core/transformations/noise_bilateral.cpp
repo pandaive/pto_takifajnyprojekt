3f5bc84fcb8831f78330901fd6c191a87bc2844f
Karolina <zonkarolina@gmail.com>
2015-11-19 04:11:56 +0100

    Zajecia 6

--- src/core/transformations/noise_bilateral.cpp
+++ src/core/transformations/noise_bilateral.cpp
@@ -21,28 +21,68 @@ PNM* NoiseBilateral::transform()
     sigma_r = getParameter("sigma_r").toInt();
     radius = sigma_d;
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    //qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+
+    if (image->format() == QImage::Format_Indexed8) {
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++) {
+                newImage->setPixel(x,y,calcVal(x,y,LChannel));
+            }
+    }
+    else { //if RGB
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++) {
+                int r = calcVal(x,y,RChannel);
+                int g = calcVal(x,y,GChannel);
+                int b = calcVal(x,y,BChannel);
+
+                QColor newPixel = QColor(r,g,b);
+                newImage->setPixel(x,y, newPixel.rgb());
+
+            }
+    }
 
     return newImage;
 }
 
 int NoiseBilateral::calcVal(int x, int y, Channel channel)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    //qDebug() << Q_FUNC_INFO << "Not implemented yet!";
 
-    return 0;
+    math::matrix<float> window;
+    int windowSize = radius;
+    window = Transformation::getWindow(x, y, windowSize, channel, RepeatEdge);
+
+    float sumUp = 0, sumDown = 0;
+
+    for (int i = 0; i < windowSize; i++)
+        for (int j = 0; j < windowSize; j++) {
+            float color = colorCloseness(window(i,j), window(windowSize/2,windowSize/2));
+            float spatial = spatialCloseness(QPoint(i,j), QPoint(windowSize/2,windowSize/2));
+
+            sumUp = sumUp + (window(i,j) * color * spatial);
+            sumDown = sumDown + (color * spatial);
+        }
+
+    return (int)sumUp/sumDown;
 }
 
 float NoiseBilateral::colorCloseness(int val1, int val2)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    //qDebug() << Q_FUNC_INFO << "Not implemented yet!";
 
-    return 0;
+    float valDiff = val1 - val2;
+    return std::exp((-1)*((valDiff * valDiff)/(2*sigma_r * sigma_r)));
+
 }
 
 float NoiseBilateral::spatialCloseness(QPoint point1, QPoint point2)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    //qDebug() << Q_FUNC_INFO << "Not implemented yet!";
 
-    return 0;
+    float xdiff = point1.x()-point2.x();
+    float ydiff = point1.y()-point2.y();
+
+    return std::exp((-1)*(((xdiff * xdiff) + (ydiff * ydiff))/(2*sigma_d*sigma_d)));
+
 }
--- src/core/transformations/noise_median.cpp
+++ src/core/transformations/noise_median.cpp
@@ -17,8 +17,24 @@ PNM* NoiseMedian::transform()
 
     PNM* newImage = new PNM(width, height, image->format());
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    if (image->format() == QImage::Format_Indexed8) {
 
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++) {
+                newImage->setPixel(x,y,getMedian(x,y,LChannel));
+            }
+    }
+    else {  //if RGB
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++) {
+                int r = getMedian(x,y,RChannel);
+                int g = getMedian(x,y,GChannel);
+                int b = getMedian(x,y,BChannel);
+
+                QColor newPixel = QColor(r,g,b);
+                newImage->setPixel(x,y, newPixel.rgb());
+            }
+    }
     return newImage;
 }
 
@@ -26,7 +42,23 @@ int NoiseMedian::getMedian(int x, int y, Channel channel)
 {
     int radius = getParameter("radius").toInt();
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    //qDebug() << Q_FUNC_INFO << "Not implemented yet!";
 
-    return 0;
+    math::matrix<float> window;
+    int windowSize = radius * 2 + 1;
+    window = Transformation::getWindow(x, y, windowSize, channel, RepeatEdge);
+
+    int valuesSize = windowSize * windowSize;
+    float* values = new float[valuesSize];
+
+    for (int i = 0; i < windowSize; i++)
+        for (int j = 0; j < windowSize; j++) {
+            values[i*windowSize+j] = window[i][j];
+        }
+
+    std::sort(values, values+valuesSize);
+
+    return (int) ((valuesSize % 2) == 0 ?
+                      (values[valuesSize/2] + values[valuesSize/2-1])/2 : floor(values[valuesSize/2]));
+
 }
