52a0c33b6b74d10b2a91920bd915fc35367d2ac2
Karolina <zonkarolina@gmail.com>
2016-01-13 20:55:30 +0100

    Zajecia 11

--- src/core/transformations/hough.cpp
+++ src/core/transformations/hough.cpp
@@ -14,10 +14,59 @@ Hough::Hough(PNM* img, ImageViewer* super) :
 }
 
 PNM* Hough::transform()
-{   
+{
     int thetaDensity = getParameter("theta_density").toInt();
+    int thetaSize = 180 * thetaDensity;
+    int width = image->width();
+    int height = image->height();
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    int pMax = sqrt(pow(width, 2) + pow(height, 2));
 
-    return 0;
+    int newWidth = thetaSize;
+    int newHeight = pMax*2+1;
+
+    //qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+
+    PNM* newImage = new PNM(newWidth, newHeight, QImage::Format_Indexed8);
+
+    math::matrix<float> hough(newWidth, newHeight);
+    for (int x = 0; x < newWidth; x++)
+        for (int y = 0; y < newHeight; y++) {
+            hough(x,y) = 0;
+        }
+
+    ConversionGrayscale* conversionGrayscale = new ConversionGrayscale(image);
+    image = conversionGrayscale->transform();
+
+    if (getParameter("skip_edge_detection").toBool() == false) {
+        EdgeLaplacian* edgeLaplacian = new EdgeLaplacian(image);
+        edgeLaplacian->setParameter("size", 3);
+        image = edgeLaplacian->transform();
+    }
+
+    for (int i = 0; i < width; i++)
+        for (int j = 0; j < height; j++) {
+            if (qGray(image->pixel(i,j)) > 0) {
+                for (int k = 0; k < thetaSize; k++) {
+                    float theta = (k * M_PI)/(thetaDensity * 180.0);
+                    float p = i * cos(theta) + j * sin(theta);
+                    hough(k, p + pMax)++;
+                }
+            }
+        }
+
+    float max;
+    for (int x = 0; x < newWidth; x++)
+        for (int y = 0; y < newHeight; y++) {
+            if (hough(x,y) > max)
+                max = hough(x,y);
+        }
+
+    for (int x = 0; x < newWidth; x++)
+        for (int y = 0; y < newHeight; y++) {
+            float v = (hough(x,y)*255)/max;
+            newImage->setPixel(x,y,v);
+        }
+
+    return newImage;
 }
--- src/core/transformations/hough_lines.cpp
+++ src/core/transformations/hough_lines.cpp
@@ -23,8 +23,55 @@ PNM* HoughLines::transform()
     bool drawWholeLines = getParameter("draw_whole_lines").toBool();
 
     PNM* newImage = new PNM(image->copy());
+    int width = image->width();
+    int height = image->height();
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+//    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+
+    EdgeLaplacian* edgeLaplacian = new EdgeLaplacian(image);
+    edgeLaplacian->setParameter("size", 3);
+    PNM* laplacianImage = edgeLaplacian->transform();
+
+    BinarizationGradient* binarizationGradient = new BinarizationGradient(laplacianImage);
+    PNM* binaryImage = binarizationGradient->transform();
+
+    Hough* hough = new Hough(binaryImage);
+    hough->setParameter("theta_density", 3);
+    hough->setParameter("skip_edge_detection", true);
+    PNM* houghImage = hough->transform();
+
+    int theta = houghImage->width();
+    int p = houghImage->height();
+
+    QPainter* qPainter = new QPainter(newImage);
+    qPainter->setPen(Qt::red);
+
+
+    for (int x = 0; x < theta; x++)
+        for (int y = 0; y < p; y++) {
+            QRgb pixel = houghImage->pixel(x,y);
+            int v = qGray(pixel);
+            if (v > threshold) {
+                int r = y - p/2;
+                float rTheta = (x/3.0)*M_PI / 180.0;
+                float sinTheta = sin(rTheta);
+                float cosTheta = cos(rTheta);
+
+                int x1 = 0;
+                int y1 = r/sinTheta;
+                int x2 = width - 1;
+                int y2 = (r - (width-1) * cosTheta)/sinTheta;
+                qPainter->drawLine(x1, y1, x2, y2);
+            }
+        }
+
+    if (!drawWholeLines) {
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++) {
+                if (qGray(binaryImage->pixel(x,y)) == 0)
+                    newImage->setPixel(x,y,image->pixel(x,y));
+            }
+    }
 
     return newImage;
 }
